aae2c336c548927cacd9debede4f02c22a272b7d
karthikeya964 <126311771+karthikeya964@users.noreply.github.com>
2025-05-05 14:41:08 +0530

    Create variables.tf

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+}
+
+variable "name" {
+  description = "EC2 instance name"
+  type        = string
+}
